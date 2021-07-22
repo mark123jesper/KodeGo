16ffbfff90e0daf39e2773f9da15b3a1606e7cfb
Mark Jesper Pillerva <mark123jesper@yahoo.com>
2021-07-22 09:32:48 +0800

    For Further Development

---
+++ Day 11 07-22-21/animation.js
@@ -0,0 +1,33 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav1() {
+    document.getElementById("mySidebar1").style.width = "100%";
+    document.getElementById("main").style.marginLeft = "100%";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav1() {
+    document.getElementById("mySidebar1").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+  
+function openNav2() {
+    document.getElementById("mySidebar2").style.width = "100%";
+    document.getElementById("main").style.marginLeft = "100%";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav2() {
+    document.getElementById("mySidebar2").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+  
+function openNav3() {
+    document.getElementById("mySidebar3").style.width = "100%";
+    document.getElementById("main").style.marginLeft = "100%";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav3() {
+    document.getElementById("mySidebar3").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
